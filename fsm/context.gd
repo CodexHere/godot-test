c300c1f1588352cf603aa234ee45f86e15bd0c67
CodexHere <codexhere@outlook.com>
2025-05-08 01:48:56 -0500

    Added state machine example

---
+++ fsm/context.gd
@@ -0,0 +1,5 @@
+# Context.gd
+extends Node
+class_name Context
+
+@export var player: NodePath
---
+++ fsm/state.gd
@@ -0,0 +1,7 @@
+# State.gd
+extends Node
+class_name State
+
+@export var state_name: String
+@export var context: Context
+@export var children: Array[State] = []
